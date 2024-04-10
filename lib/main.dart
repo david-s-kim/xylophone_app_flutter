fd1eed283f58216abd8ecb1dff7e0082156c6ea5
David Kim <mycodingtool9@gmail.com>
2024-04-10 23:28:08 +0900

    - feat: Sound pool 라이브러리 준비

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,6 @@ class MyApp extends StatelessWidget {
 }
 
 class XylophoneApp extends StatefulWidget {
-
-
   const XylophoneApp({super.key});
 
   @override
@@ -36,6 +34,7 @@ class _XylophoneAppState extends State<XylophoneApp> {
   Soundpool pool = Soundpool.fromOptions(options: SoundpoolOptions.kDefault);
 
   List<int> _soundIds = [];
+  bool _isLoading = true;
 
   @override
   void initState() {
@@ -91,68 +90,78 @@ class _XylophoneAppState extends State<XylophoneApp> {
         .then((soundData) => pool.load(soundData));
 
     _soundIds.add(soundId);
+
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeLeft
-    ]);
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('실로폰'),
         backgroundColor: Colors.green,
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: gunban('도', Colors.red),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 24.0),
-            child: gunban('레', Colors.orange),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 32.0),
-            child: gunban('미', Colors.deepOrangeAccent),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40.0),
-            child: gunban('파', Colors.blue),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 48.0),
-            child: gunban('솔', Colors.green),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 56.0),
-            child: gunban('라', Colors.purple),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 64.0),
-            child: gunban('시', Colors.tealAccent),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 72.0),
-            child: gunban('도', Colors.indigoAccent),
-          ),
-        ],
-      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator())
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: gunban('도', Colors.red, _soundIds[0]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: gunban('레', Colors.orange, _soundIds[1]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 32.0),
+                  child: gunban('미', Colors.deepOrangeAccent, _soundIds[2]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 40.0),
+                  child: gunban('파', Colors.blue, _soundIds[3]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 48.0),
+                  child: gunban('솔', Colors.green, _soundIds[4]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 56.0),
+                  child: gunban('라', Colors.purple, _soundIds[5]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 64.0),
+                  child: gunban('시', Colors.tealAccent, _soundIds[6]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 72.0),
+                  child: gunban('도', Colors.indigoAccent, _soundIds[7]),
+                ),
+              ],
+            ),
     );
   }
 
-  Widget gunban(String text, Color color) {
-    return Container(
-      width: 50,
-      height: double.infinity,
-      color: color,
-      child: Center(
-        child: Text(
-          text,
-          style: const TextStyle(color: Colors.white),
+  Widget gunban(String text, Color color, int soundId) {
+    return GestureDetector(
+      onTap: () {
+        pool.play(soundId);
+      },
+      child: Container(
+        width: 50,
+        height: double.infinity,
+        color: color,
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
